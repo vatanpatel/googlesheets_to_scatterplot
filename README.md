e9b15413e652b7b7a7ac4082bc8e5a77f57fcce4
vatanpatel <vatanpatel93@gmail.com>
2020-10-09 16:41:32 +0530

    Final fixes

--- README.md
+++ README.md
@@ -2,9 +2,7 @@ googlesheets_to_scatterplot is a Python module for creating plots directly from 
 
 The project was started in 2020 by Vatan Patel as a pre-process task for Greendeck.
 
-### Function to plot:
-googlesheets_to_scatterplot.plot(spreadsheet_name, xcol, ycol, sheet_number)
-
+#
 ### Dependencies:
 
 - pandas
@@ -13,6 +11,7 @@ googlesheets_to_scatterplot.plot(spreadsheet_name, xcol, ycol, sheet_number)
 - gspread
 - oauth2client
 
+#
 ### Setup:
 
  - Now you need to set up your sharing options. You will need to generate Signed Credentials, something that sounds more difficult than it is. Navigate to the Google Developers Console and create a new project (or use an existing one).
@@ -31,8 +30,22 @@ googlesheets_to_scatterplot.plot(spreadsheet_name, xcol, ycol, sheet_number)
     - Finally, open the file and look for client_email. This should be the name of your project at appspot.gserviceaccount.com. Share your Google Sheet with this email address (Top Right > Share > Enter Email).
     
  Now we are good to go. You can now use the module.
+ #
+ ### Installation:
+ 
+ If you already have a working installation of numpy, pandas, matplotlib, seaborn, oauth2client and gspread the easiest way to install googlesheets_to_scatterplot is using pip:
+   
+```bash
+    $ pip install googlesheets-to-scatterplot==0.0.8
+```
 
-### Parameters:
+#
+### Function to plot:
+
+googlesheets_to_scatterplot.plot(spreadsheet_name, xcol, ycol, sheet_number)
+
+#
+### Parameters for googlesheets_to_scatterplot.plot():
 
 - spreadsheet_name (str, required): Write the name of the google spreadsheet you want to create plots from. Make sure the name matches exactly.
 
@@ -40,5 +53,29 @@ googlesheets_to_scatterplot.plot(spreadsheet_name, xcol, ycol, sheet_number)
 
 - ycol (str, required): The column name of the column you want on the y-axis.
 
-- sheet_number (int, default = 1): This is the sheet number in your spreadsheet on which the table is present).
+- sheet_number (int, default = 1): This is the sheet number in your spreadsheet on which the table is present.
 
+#
+### Imortant Links:
+URL: https://pypi.org/project/googlesheets-to-scatterplot/0.0.12/
+
+# 
+### Changelog:
+
+- 0.0.4 (08/10/2020)
+    - First Release
+
+- 0.0.7 (09/10/2020)
+    - Second Release
+       - We remove the need for providing the client name
+       - Default sheet number is takes as 1
+       - We updated the README file.
+       - We add proper comments to our library for people to refer later
+       - Made changes to the title
+       - We removed changelog from long_description
+       
+- 0.0.12 (09/10/2020)
+    - Third release
+        - We added changelog and other descriptions to README
+
+#
--- setup.py
+++ setup.py
@@ -1,23 +1,24 @@
 from setuptools import setup, find_packages
 
 # We read the contents of our README file
-with open("README.md", "r") as fh:
+with open("README.md") as fh:
     long_description = fh.read()              # We read the contents of our README file into long_discription
 
 # We define the classifiers
 classifiers = [
-    'Intended Audience :: Education',         # We created this module to serve educational purpose
-    'Operating System :: OS Independent',     # We can use this module on any OS
-    'License :: OSI Approved :: MIT License', # The most widely used open source licence
-    'Programming Language :: Python :: 3'     # The programming language for the module is Python
+    'Intended Audience :: Education',          # We created this module to serve educational purpose
+    'Operating System :: OS Independent',      # We can use this module on any OS
+    'License :: OSI Approved :: MIT License',  # The most widely used open source licence
+    'Programming Language :: Python :: 3'      # The programming language for the module is Python
 ]
 
 # Now we are good to setup our module
 setup(
     name='googlesheets_to_scatterplot',                                                         # We define the name of our module
-    version='0.0.7',                                                                            # We define the version. We update this everytime we make a change
+    version='0.0.12',                                                                           # We define the version. We update this everytime we make a change
     description='We can create plots directly from Google Sheets',                              # We define the short discription
-    long_description= long_description, # + '\n\n' + open('CHANGELOG.txt').read(),                 # We define the long distribution by adding long_discription variable and add our CHANGELOG to it
+    long_description= long_description,                                                         # We define the long distribution by adding long_discription variable
+    long_description_content_type="text/markdown",                                              # We set the content type of our README file
     url='https://github.com/vatanpatel/googlesheets_to_scatterplot',                            # We add the git url for reference
     author='Vatan Patel',                                                                       # We add the author name
     author_email='i12vatanp@iimidr.ac.in',                                                      # We add the author email for people to get in touch
@@ -25,6 +26,5 @@ setup(
     classifiers=classifiers,                                                                    # We add our defined classifiers here
     keywords='googlesheets',                                                                    # We add keywords people can use to search this module
     packages=find_packages(),                                                                   # We want our module to find the packages required to run
-    install_requires=['markdown','pandas', 'matplotlib', 'seaborn', 'gspread', 'oauth2client']  # We define the modules required to run our module
+    install_requires=['markdown', 'pandas', 'matplotlib', 'seaborn', 'gspread', 'oauth2client']  # We define the modules required to run our module
 )
-
